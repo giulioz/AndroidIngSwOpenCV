ba9889173b5b9d2981df0ec6fab0663411c5ced7
Giulio Zausa <mail.zausa.giulio@gmail.com>
2019-10-28 11:11:16 +0100

    WIP

--- app/src/main/java/it/unive/dais/findmyballs/MainActivity.java
+++ app/src/main/java/it/unive/dais/findmyballs/MainActivity.java
@@ -3,9 +3,18 @@ package it.unive.dais.findmyballs;
 import androidx.appcompat.app.AppCompatActivity;
 
 import android.os.Bundle;
+import android.os.Handler;
 import android.util.Log;
 import android.view.SurfaceView;
+import android.view.View;
 import android.view.WindowManager;
+import android.widget.LinearLayout;
+
+import net.sourceforge.zbar.Config;
+import net.sourceforge.zbar.Image;
+import net.sourceforge.zbar.ImageScanner;
+import net.sourceforge.zbar.Symbol;
+import net.sourceforge.zbar.SymbolSet;
 
 import org.opencv.android.CameraBridgeViewBase;
 import org.opencv.android.OpenCVLoader;
@@ -15,11 +24,16 @@ import org.opencv.core.Point;
 import org.opencv.core.Scalar;
 import org.opencv.imgproc.Imgproc;
 
+import me.dm7.barcodescanner.zbar.BarcodeFormat;
+import me.dm7.barcodescanner.zbar.Result;
+import me.dm7.barcodescanner.zbar.ZBarScannerView;
+
 public class MainActivity extends AppCompatActivity {
 
     private String TAG = "AndroidIngSwOpenCV";
 
     private CameraBridgeViewBase mOpenCvCameraView;
+    private ZBarScannerView mScannerView;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
@@ -40,15 +54,16 @@ public class MainActivity extends AppCompatActivity {
         mOpenCvCameraView = findViewById(R.id.HelloOpenCvView);
         mOpenCvCameraView.setVisibility(SurfaceView.VISIBLE);
         mOpenCvCameraView.setMaxFrameSize(320, 240);
+        mOpenCvCameraView.disableFpsMeter();
         mOpenCvCameraView.setCvCameraViewListener(new CameraBridgeViewBase.CvCameraViewListener2() {
             @Override
             public void onCameraViewStarted(int width, int height) {
-                Log.e(TAG, "Camera Started");
+                Log.d(TAG, "Camera Started");
             }
 
             @Override
             public void onCameraViewStopped() {
-                Log.e(TAG, "Camera Stopped");
+                Log.d(TAG, "Camera Stopped");
             }
 
             // Viene eseguito ad ogni frame, con inputFrame l'immagine corrente
@@ -56,23 +71,80 @@ public class MainActivity extends AppCompatActivity {
             public Mat onCameraFrame(CameraBridgeViewBase.CvCameraViewFrame inputFrame) {
                 // Salva il frame corrente su un oggetto Mat, ossia una matrice bitmap
                 Mat frame = inputFrame.rgba();
+                // Crea una nuova Mat per effettuare elaborazioni
+                Mat median = new Mat();
 
-                // Crea una nuova Mat a partire dal frame corrente per effettuare elaborazioni
-                Mat median = frame.t();
+                // Converte il formato colore da BGR a RGB
+                Imgproc.cvtColor(frame, median, Imgproc.COLOR_BGR2RGB);
 
-                // Ruota il frame per farlo funzionare in modalità portrait
-                Core.flip(median, median, 1);
-                Imgproc.resize(median, median, frame.size());
+                // Effettua un filtro mediana di dimensione 5 sull'immagine
+                Imgproc.medianBlur(frame, median, 5);
 
                 // Disegna una linea in mezzo allo schermo
                 Imgproc.line(median, new Point(0, 120), new Point(320, 120), new Scalar(0, 255, 0), 1);
 
+                ImageScanner mScanner = new ImageScanner();
+                mScanner.setConfig(0, Config.X_DENSITY, 3);
+                mScanner.setConfig(0, Config.Y_DENSITY, 3);
+                mScanner.setConfig(Symbol.NONE, Config.ENABLE, 0);
+                for(BarcodeFormat format : BarcodeFormat.ALL_FORMATS) {
+                    mScanner.setConfig(format.getId(), Config.ENABLE, 1);
+                }
+
+                Image imageToScan = new Image(frame.cols(), frame.rows(), "Y800");
+                byte[] return_buff = new byte[(int) (frame.total() *
+                        frame.channels())];
+                frame.get(0, 0, return_buff);
+                imageToScan.setData(return_buff);
+                int qrResult = mScanner.scanImage(imageToScan);
+                if (qrResult != 0) {
+                    SymbolSet sym = mScanner.getResults();
+                    for (Symbol s : sym) {
+                        Log.d(TAG, "Found QR: " + s.getData());
+                    }
+                }
+
                 // Ritorna il frame da visualizzare a schermo
-                return median;
+                return frame;
             }
         });
 
         // Abilita la visualizzazione dell'immagine sullo schermo
         mOpenCvCameraView.enableView();
+
+        mScannerView = new ZBarScannerView(this);
+//        mScannerView.setVisibility(View.INVISIBLE);
+//        LinearLayout layout = findViewById(R.id.layout);
+//        layout.addView(mScannerView);
+    }
+
+    @Override
+    public void onResume() {
+        super.onResume();
+
+        mScannerView.setResultHandler(new ZBarScannerView.ResultHandler() {
+            private final ZBarScannerView.ResultHandler _this = this;
+
+            @Override
+            public void handleResult(Result rawResult) {
+                Log.d(TAG, "Found QR: " + rawResult.getContents());
+
+                Handler handler = new Handler();
+                handler.postDelayed(new Runnable() {
+                    @Override
+                    public void run() {
+                        mScannerView.resumeCameraPreview(_this);
+                    }
+                }, 2000);
+            }
+        });
+
+        mScannerView.startCamera();
+    }
+
+    @Override
+    public void onPause() {
+        super.onPause();
+        mScannerView.stopCamera();
     }
 }

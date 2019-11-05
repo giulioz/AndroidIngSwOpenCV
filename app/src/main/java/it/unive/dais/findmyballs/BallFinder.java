23db6ff55299cfb02d1a1f95e4c271ab26df6109
raining-blood <m.alex98@hotmail.it>
2019-11-05 12:57:35 +0100

    Fixed BallFinder, added debug flag

--- app/src/main/java/it/unive/dais/findmyballs/BallFinder.java
+++ app/src/main/java/it/unive/dais/findmyballs/BallFinder.java
@@ -10,6 +10,8 @@ import org.opencv.core.MatOfPoint2f;
 import org.opencv.core.Point;
 import org.opencv.core.Scalar;
 import org.opencv.imgproc.Imgproc;
+import org.opencv.core.Size;
+import org.opencv.core.CvType;
 
 public class BallFinder {
     private float view_ratio = 0.0f;
@@ -24,23 +26,35 @@ public class BallFinder {
     private int yellow_lower = 16;
     private int yellow_upper = 25;
 
+    private boolean debug = false;
+
     private Mat frame;
 
     public BallFinder(Mat frame) {
         this.frame = frame.clone();
     }
 
+    public BallFinder(Mat frame, boolean debug) {
+        this(frame);
+
+        if (debug) {
+            this.frame = frame;
+            this.debug = true;
+        }
+    }
+
     public Mat findBalls() {
         Mat hsv = new Mat();
         List <Mat> split_hsv = new ArrayList<>();
 
-        Imgproc.cvtColor(frame, hsv, Imgproc.COLOR_BGR2HSV);
+        Imgproc.cvtColor(frame, hsv, Imgproc.COLOR_RGB2HSV);
         Core.split(hsv, split_hsv);
 
         Mat mask_sat = new Mat();
         Imgproc.threshold(split_hsv.get(1), mask_sat, sat_lower, sat_upper, Imgproc.THRESH_BINARY);
 
-        // TODO: add morphing to mask_sat
+        Mat kernel = new Mat(new Size(3, 3), CvType.CV_8UC1, new Scalar(255));
+        Imgproc.morphologyEx(mask_sat, mask_sat, Imgproc.MORPH_OPEN, kernel);
 
         Mat hue = split_hsv.get(0);
         Mat mask_red = new Mat();
@@ -63,8 +77,10 @@ public class BallFinder {
 
         Imgproc.findContours(mask, contours, hierarchy, Imgproc.RETR_EXTERNAL, Imgproc.CHAIN_APPROX_SIMPLE);
 
-        for (int i = 0; i < contours.size(); i++)
-            Imgproc.drawContours(frame, contours, i, new Scalar(255, 0, 0), 1);
+        if (debug) {
+            for (int i = 0; i < contours.size(); i++)
+                Imgproc.drawContours(frame, contours, i, new Scalar(255, 0, 0), 1);
+        }
 
         float[] radius = new float[1];
         Point center = new Point();
@@ -74,15 +90,16 @@ public class BallFinder {
             Imgproc.minEnclosingCircle(new MatOfPoint2f(c.toArray()), center, radius);
 
             if (center.y > frame.height() * view_ratio && Imgproc.contourArea(c) > min_area) {
+                // TODO: add color mean for area_hue
                 int area_hue = (int) hue.get((int) center.y, (int) center.x)[0];
                 Scalar color;
 
                 if (area_hue >= red_lower && area_hue <= red_upper)
-                    color = new Scalar(0, 0, 255);
-                else if (area_hue >= blue_lower && area_hue <= blue_upper)
                     color = new Scalar(255, 0, 0);
+                else if (area_hue >= blue_lower && area_hue <= blue_upper)
+                    color = new Scalar(0, 0, 255);
                 else if (area_hue >= yellow_lower && area_hue <= yellow_upper)
-                    color = new Scalar(0, 255, 255);
+                    color = new Scalar(255, 255, 0);
                 else
                     color = new Scalar(0, 0, 0);
 
